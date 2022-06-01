0249591af5aa378e3ce80483f044bff5f6e270e7
Konstantinos Kostarellis <dev@kostarellis.com>
2022-06-01 12:22:18 +0200

    adds d3 chart logic from https://github.com/higsch/simple-svelte-chart into the fresh sveltekit project

---
+++ src/components/Chart.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { scaleLinear } from "d3";
+
+  import XAxis from "./XAxis.svelte";
+  import YAxis from "./YAxis.svelte";
+  import Datapoint from "./Datapoint.svelte";
+  import Line from "./Line.svelte";
+
+  export let data;
+  export let xRange;
+  export let yRange;
+
+  const padding = 50;
+
+  let width;
+  let height;
+
+  $: xScale = scaleLinear()
+    .domain(xRange)
+    .range([padding, width - padding]);
+
+  $: yScale = scaleLinear()
+    .domain(yRange)
+    .range([height - padding, padding]);
+
+  $: renderedData = data.map((d) => {
+    return {
+      x: xScale(d.x),
+      y: yScale(d.y),
+    };
+  });
+</script>
+
+<div class="chart" bind:clientWidth={width} bind:clientHeight={height}>
+  {#if width && height}
+    <svg {width} {height}>
+      <XAxis scale={xScale} y={height - padding} />
+      <YAxis scale={yScale} x={padding} />
+      <Line data={renderedData} />
+      {#each renderedData as { x, y }}
+        <Datapoint {x} {y} />
+      {/each}
+    </svg>
+  {/if}
+</div>
+
+<style>
+  .chart {
+    flex: 1;
+    width: 100%;
+    overflow: hidden;
+  }
+
+  /* svg {
+        background: red;
+    } */
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import data from "../data";
+
+  import Chart from "../components/Chart.svelte";
+  import Selector from "../components/Selector.svelte";
+
+  let selectedOption = "a";
+
+  $: selectedData = data[selectedOption].sort((a, b) => (a.x < b.x ? 1 : -1));
+</script>
+
+<div class="wrapper">
+  <Chart data={selectedData} xRange={[0, 20]} yRange={[0, 16]} />
+  <Selector options={Object.keys(data)} bind:selected={selectedOption} />
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+  }
+</style>
